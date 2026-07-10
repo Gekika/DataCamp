e0918015ec89a9e543b4df56414cc5f107dd78c3
Gekika <gkkaranja2@gmail.com>
2026-07-10 03:09:55 +0300

    Added a range of Ks to evaluate which one performs best

--- AIEngineer/supervised-learning/measuring-accuracy/accuracy.py
+++ AIEngineer/supervised-learning/measuring-accuracy/accuracy.py
@@ -43,3 +43,25 @@ model = knn.fit(X_train, y_train)
 
 # Print the accuracy
 print(knn.score(X_test, y_test))
+
+
+
+# sample
+# how we would have a range of k nad evaluate them 
+# Create neighbors
+neighbors = np.arange(1, 13)
+train_accuracies = {}
+test_accuracies = {}
+
+for neighbor in neighbors:
+  
+	# Set up a KNN Classifier
+	knn = KNeighborsClassifier(n_neighbors=neighbor)
+  
+	# Fit the model
+	knn.fit(X_train, y_train)
+  
+	# Compute accuracy
+	train_accuracies[neighbor] = knn.score(X_train, y_train)
+	test_accuracies[neighbor] = knn.score(X_test, y_test)
+print(neighbors, '\n', train_accuracies, '\n', test_accuracies)
