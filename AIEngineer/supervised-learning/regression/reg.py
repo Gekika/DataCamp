c61da1759e390f042c9a778ea38b687eecc5bd2b
Gekika <gkkaranja2@gmail.com>
2026-07-12 00:08:56 +0300

    we haev split our data to the fearures and targets

--- AIEngineer/supervised-learning/regression/reg.py
+++ AIEngineer/supervised-learning/regression/reg.py
@@ -1,10 +1,10 @@
 import numpy as np
 
 # Create X from the radio column's values
-X = sales_df.drop("radio", axis=1).values
+X = sales_df["radio"].values
 
 # Create y from the sales column's values
-y = sales_df["sales"]
+y = sales_df["sales"].values
 
 # Reshape X
 X = X.reshape(-1,1)
